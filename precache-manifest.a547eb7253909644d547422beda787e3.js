69acd46211c1864d9bc50837e737da8aa53ee98c
Kikulichka <kikulichka.jimenez@gmail.com>
2020-08-31 19:48:34 +0200

    V1.0

---
+++ precache-manifest.a547eb7253909644d547422beda787e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e29e04c1df1e6592ff0ce11c09ce63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b2957814f21c9c4b47c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "04f6b7fcbac32eee4e5d",
+    "url": "/static/js/2.71e01baa.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.71e01baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2957814f21c9c4b47c",
+    "url": "/static/js/main.dfea0e87.chunk.js"
+  },
+  {
+    "revision": "04536ce8de1b7fcdd528",
+    "url": "/static/js/runtime-main.8f8f3c52.js"
+  },
+  {
+    "revision": "acda6c83dea5ad1d66b89e4c04f0d0a7",
+    "url": "/static/media/ground.acda6c83.jpg"
+  },
+  {
+    "revision": "3cd330cc5bb9e4de9c7f6211487b2fc8",
+    "url": "/static/media/heightMap.3cd330cc.png"
+  }
+]);
---
+++ precache-manifest.e18d443926f8c79dffc57e571be0ce3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51011be731d972aba1c47df35539cff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09bd8de023a7ba00d780",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2dac8948b88c97303ecb",
+    "url": "/static/js/2.780bad72.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.780bad72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09bd8de023a7ba00d780",
+    "url": "/static/js/main.2b4fa2e2.chunk.js"
+  },
+  {
+    "revision": "04536ce8de1b7fcdd528",
+    "url": "/static/js/runtime-main.8f8f3c52.js"
+  },
+  {
+    "revision": "acda6c83dea5ad1d66b89e4c04f0d0a7",
+    "url": "/static/media/ground.acda6c83.jpg"
+  },
+  {
+    "revision": "3cd330cc5bb9e4de9c7f6211487b2fc8",
+    "url": "/static/media/heightMap.3cd330cc.png"
+  }
+]);
